d5758c61dc3feb50f143c3a9d4ab3a4fdb8b4283
Louie Alarcon <tumbando@gmail.com>
2016-06-23 14:37:57 -0500

    finished most of button layout

---
+++ main.js
@@ -0,0 +1,4 @@
+//remember keystrokes(numbers/operations)
+//operation buttons must operate accordingly
+//show answer in output
+//need 16 boxes, 10 for 0-9, 4 for operations, 1 for equals, and 1 for output
